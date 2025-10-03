d25450136325f386fb13308ca36ad5025d4fea67
Aayush895 <aayush895@gmail.com>
2025-10-03 22:00:54 +0530

    Created a socket connection between client and server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,22 @@
 import express, { urlencoded } from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 import cors from 'cors';
-
 import { PORT } from './config/serverConfig.js';
 import apiRouter from '../src/routes/index.js';
+
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  },
+});
+
+io.on('connection', () => {
+  console.log('A user is connected');
+});
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -11,6 +24,6 @@ app.use(cors());
 
 app.use('/api', apiRouter);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
+import { io } from 'socket.io-client'
 import Router from './Router'
+
+const socket = io('http://localhost:3000')
 function App() {
   return <Router />
 }
